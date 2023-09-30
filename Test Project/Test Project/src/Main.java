58622a887962639c4459334c84f529fe7fa49d44
Sukanvisa Pearyoo <Sukanvisa.pearyoo@gmail.com>
2023-09-29 20:10:55 -0700

    Committing my initial file

--- Test Project/Test Project/src/Main.java
+++ Test Project/Test Project/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hi Josh. Your video was great. " +
+                "I had hard time figuring this out when I was in data structure class. Thank you");
     }
 }
