c390c6bb4faddc83a7964a8ae7d8a0a1c1ef7a5f
Sukanvisa Pearyoo <Sukanvisa.pearyoo@gmail.com>
2023-09-29 20:11:23 -0700

    Committing my changes

--- Test Project/Test Project/src/Main.java
+++ Test Project/Test Project/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi Josh. Your video was great. " +
-                "I had hard time figuring this out when I was in data structure class. Thank you");
+                "I had a hard time figuring this out when I was in data structure class. Thank you");
     }
 }
